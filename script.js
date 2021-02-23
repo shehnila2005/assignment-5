c92c13b633d90859923edb9a60c709aaf1f14e38
Shehnila Azmat <shehnila.azmat@ocsbstudent.ca>
2021-02-23 20:02:13 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+let r = 0
+let answer = 0
+document.getElementById('button').addEventListener('click', area)
+document.getElementById('button2').addEventListener('click', circumference)
+function area () {
+  r = document.getElementById('input').value
+  r = parseInt(r)
+  answer = Math.PI * (r * r)
+  document.getElementById('answer').innerHTML = Math.round(answer)
+}
+function circumference () {
+circumference = parseInt(circumference)
+r = document.getElementById('input2').value
+r = parseInt(r)
+answer = 2 * r
+document.getElementById('answer').innerHTML = Math.round(answer)
+}
